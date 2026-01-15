afbd999d34c169efd1520d8a3b2a6d8ffb8cbd8d
jiucai233 <jiucai@yonsei.ac.kr>
2026-01-15 09:55:48 +0900

    passed all the tests

--- 2(2)-WEB/app/user/user_service.py
+++ 2(2)-WEB/app/user/user_service.py
@@ -9,26 +9,26 @@ class UserService:
         '''
         Login user with email and password
         compare with stored user data in the repository
-        if the data cannot match, raise an exception
+        if the data cannot match, raise an ValueError
         '''
         user_email = user_login.email
         user_password = user_login.password
         user = self.repo.get_user_by_email(user_email)
         if not user:
-            raise Exception("User does not exist")
+            raise ValueError("User not Found.")
         if user.password != user_password:
-            raise Exception("Incorrect password")
+            raise ValueError("Invalid ID/PW")
         return user
         
     def register_user(self, new_user: User) -> User:
         '''
-        Register a new user, if the email already exists, raise an exception
+        Register a new user, if the email already exists, raise an ValueError
         Otherwise, save the new user to the repository
         '''
         new_user_email = new_user.email
         existing = self.repo.get_user_by_email(new_user_email)
         if existing:
-            raise Exception("User already exists")
+            raise ValueError("User already Exists.")
 
         self.repo.save_user(new_user)
    
@@ -36,19 +36,19 @@ class UserService:
 
     def delete_user(self, email: str) -> User:
         '''
-        Delete a user by email, if the email does not exist, raise an exception
+        Delete a user by email, if the email does not exist, raise an ValueError
         Otherwise, delete the user from the repository
         '''
         existing = self.repo.get_user_by_email(email)
         if not existing:
-            raise Exception("User not exists")
+            raise ValueError("User not Found.")
 
         deleted_user = self.repo.delete_user(existing)
         return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
         '''
-        Update a user's password, if the email does not exist, raise an exception
+        Update a user's password, if the email does not exist, raise an ValueError
         Otherwise, update the user's password in the repository
         '''
         # get var
@@ -58,7 +58,7 @@ class UserService:
         # query
         user_in_db = self.repo.get_user_by_email(user_email)
         if not user_in_db:
-            raise Exception("User not exists")
+            raise ValueError("User not Found.")
 
         # update
         user_in_db.password = new_password
